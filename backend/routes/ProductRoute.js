e6f7ad6f5a8b6a6182385b838e7abb8296700623
NiditPatel <criticalgaming2611@gmail.com>
2023-09-26 01:37:17 +0530

    changes --nv

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -57,24 +57,131 @@ router.delete("/product/:id",
     }
 });
 
+// router.get("/search",
+//     async (req, res) => {
+//         const {productName,supplierName,storeName} = req.query;
+//         const product = productName ? productName :''
+//         const supplier = supplierName ? supplierName :''
+//         const store = storeName? storeName : '' 
+
+//     const data = await Products.find({$or:[
+//         {productName: {'$regex':product,'$options':'i'}},
+//     {'store.storeName':{'$regex':store,'$options':'i'}},
+//     {'supplier.supplierName':{'$regex':supplier,'$options':'i'}}
+// ]
+// })
+//     .populate({path:'store',select:'storeName contactNumber -_id'})
+//     .populate({path:'supplier',supplierName:{'$regex':supplier,'$options':'i'},select:'supplierName contactNumber -_id'});
+   
+//         res.status(200).json({
+//             sucess: true,
+//             data:data
+//         })
+// });
+
 router.get("/search",
     async (req, res) => {
-        const {productName,supplierName,storeName} = req.query;
-        const product = productName ? productName :''
-        const supplier = supplierName ? supplierName :''
-        const store = storeName? storeName : '' 
+        // const {productName,supplierName,storeName} = req.query;
+        const { productName,supplierName,storeName, offset, limit, sort_by, order } = req.query;
+        const product = productName !== undefined ? productName :''
 
-    const data = await Products.find({$and:[{productName: {'$regex':product,'$options':'i'}},
-    {'store.storeName':{'$regex':store,'$option':'i'}},
-    {'supplier.supplierName':{'$regex':supplier,'$option':'i'}}]})
-    .populate({path:'store',storeName:{'$regex':store,'$option':'i'},select:'storeName contactNumber -_id'})
-    .populate({path:'supplier',supplierName:{'$regex':supplier,'$option':'i'},select:'supplierName contactNumber -_id'});
+
+
+        const page_limit = ((limit !== undefined && limit.length > 0) ? parseInt(limit) : 5);
+        const page_no = ((offset !== undefined && offset.length > 0) ? parseInt(offset) - 1 : 0);
+        const sort_order = ((order !== undefined && order.length > 0) ? parseInt(order) : 1);
+        const sort_field = ((sort_by !== undefined && sort_by.length > 0) ? sort_by : '_id');
+
+        const filterQuery =  {productName:{'$regex':product,'$options':'i'}}
+       const lookupQuery1 = [
+            {
+                $lookup: {
+                    from: 'suppliers',
+                    localField: 'supplier',
+                    foreignField: '_id',
+                    as: 'supplier',
+                    pipeline: [
+                        {
+                            $project: {
+                                supplierName: 1,
+                                contactNumber:1,
+                                _id:0
+                            }
+                        }
+                    ]
+                },
+            },
+            {
+                $unwind: {
+                    path: '$supplier',
+                    // for not showing not matched doc 
+                     preserveNullAndEmptyArrays: false
+                }
+            }
+        ]
+
+        const lookupQuery2 = [
+            {
+                $lookup: {
+                    from: 'stores',
+                    localField: 'store',
+                    foreignField: '_id',
+                    as: 'store',
+                    pipeline: [
+                        {
+                            $project: {
+                                storeName: 1,
+                                contactNumber:1,
+                                _id:0
+                            }
+                        }
+                    ]
+                },
+            },
+            {
+                $unwind: {
+                    path: '$store',
+                    // for not showing not matched doc 
+                     preserveNullAndEmptyArrays: false
+                }
+            }
+        ]
+        const supplierFilterQuery = supplierName ? supplierName :''
+        const storeFilterQuery =storeName? storeName : '' ;
+
+    const data = await Products.aggregate([
+        { $match: filterQuery},
+         ...lookupQuery1,
+         ...lookupQuery2,
+         {$match:{$and:
+            [
+                {'supplier.supplierName': {'$regex':supplierFilterQuery ,'$options':'i'}},
+                {'store.storeName':{'$regex':storeFilterQuery,'$options':'i'}}
+            ]
+        }},
+            {
+                $facet: {
+                    metadata: [
+                        {
+                            $group: {
+                                _id: null,
+                                total: { $sum: 1 }
+                            }
+                        },
+                    ],
+                    data: [
+                        { $sort: { [sort_field]: sort_order } },
+                        { $skip: page_limit * page_no },
+                        { $limit: page_limit },
+                    ]
+                }
+            },
+    ])
    
         res.status(200).json({
             sucess: true,
             data:data
         })
 });
-
 
 module.exports  = router;
