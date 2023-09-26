1afde7ce0853be75bf0cc88ff7a5aca08cbe50da
NiditPatel <criticalgaming2611@gmail.com>
2023-09-27 03:30:11 +0530

    changes --nv

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,24 +23,34 @@ export function StickyNavbar() {
 
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal">
-        <Link to="/suppliers">
-          Supplier
-        </Link>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal">
-        <Link to="/">
-          Home
-        </Link>
-      </Typography>
+      <Button
+            variant="gradient"
+            size="sm"
+            onClick={((e) => { navigate("/stores"); })}
+          >
+            <span>Store</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            onClick={((e) => { navigate("/suppliers"); })}
+          >
+            <span>Supplier</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            onClick={((e) => { navigate("/"); })}
+          >
+            <span>Home</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            onClick={((e) => { navigate("/cart"); })}
+          >
+            <span>Cart</span>
+          </Button>
     </ul>
   );
 
@@ -57,7 +67,7 @@ export function StickyNavbar() {
           </Link>
         </Typography>
         <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
+          {/* <div className="mr-4 hidden lg:block">{navList}</div> */}
           <Button
             variant="gradient"
             size="sm"
@@ -65,6 +75,22 @@ export function StickyNavbar() {
             onClick={((e) => { navigate("/stores"); })}
           >
             <span>Store</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            className="hidden lg:inline-block"
+            onClick={((e) => { navigate("/suppliers"); })}
+          >
+            <span>Supplier</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            className="hidden lg:inline-block"
+            onClick={((e) => { navigate("/"); })}
+          >
+            <span>Home</span>
           </Button>
           <Button
             variant="gradient"
@@ -115,9 +141,7 @@ export function StickyNavbar() {
       </div>
       <Collapse open={openNav}>
         {navList}
-        <Button variant="gradient" size="sm" fullWidth className="mb-2">
-          <span>Buy Now</span>
-        </Button>
+         
       </Collapse>
     </Navbar>
   );
