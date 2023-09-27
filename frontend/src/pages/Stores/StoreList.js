7818d602d61006bf3efcd0fff31890b0485d1bf3
Jatinvaish <1195jatinvaishnav@gmail.com>
2023-09-27 21:54:14 +0530

    jv

--- frontend/src/pages/Stores/StoreList.js
+++ frontend/src/pages/Stores/StoreList.js
@@ -57,7 +57,8 @@ const Store = () => {
 
   useEffect(() => {
     getData();
-  }, [searchData]);
+    getTotalStores();
+  }, [searchData,page_Index]);
 
   const handleDelete = async (e, id) => {
     try {
@@ -195,38 +196,6 @@ const Store = () => {
               </tbody>
             </table>
           </CardBody>
-
-          {/* <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-          <Button variant="outlined" size="sm">
-            Previous
-          </Button>
-          <div className="flex items-center gap-2">
-            <IconButton variant="outlined" size="sm">
-              1
-            </IconButton>
-            <IconButton variant="text" size="sm">
-              2
-            </IconButton>
-            <IconButton variant="text" size="sm">
-              3
-            </IconButton>
-            <IconButton variant="text" size="sm">
-              ...
-            </IconButton>
-            <IconButton variant="text" size="sm">
-              8
-            </IconButton>
-            <IconButton variant="text" size="sm">
-              9
-            </IconButton>
-            <IconButton variant="text" size="sm">
-              10
-            </IconButton>
-          </div>
-          <Button variant="outlined" size="sm">
-            Next
-          </Button>
-        </CardFooter> */}
           <CardFooter className="pt-0 ">
             <Stack>
               <Pagination
